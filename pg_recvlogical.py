f7952648cd9b9a4df60f623651015c67739791ab
snarayanank2 <siva@fyle.in>
2020-11-16 22:40:09 +0530

    Support whitelist and blacklist and do transformation

--- pg_recvlogical.py
+++ pg_recvlogical.py
@@ -6,20 +6,82 @@ from psycopg2.extras import LogicalReplicationConnection
 import pika
 import click
 import logging
+import re
 
 logger = logging.getLogger(__name__)
 
-def consume_stream_stdout(msg):
-    logger.info('got message %s', msg.payload)
+def insert_event(change):
+    event = {
+        'kind': 'insert',
+        'table': '{0}.{1}'.format(change['schema'], change['table']),
+        'types': dict(zip(change['columnnames'], change['columntypes'])),
+        'old': None,
+        'new': dict(zip(change['columnnames'], change['columnvalues']))
+    }
+    return event
+
+def update_event(change):
+    event = {
+        'kind': 'update',
+        'table': '{0}.{1}'.format(change['schema'], change['table']),
+        'types': dict(zip(change['columnnames'], change['columntypes'])),
+        'old': dict(zip(change['oldkeys']['keynames'], change['oldkeys']['keyvalues'])),
+        'new': dict(zip(change['columnnames'], change['columnvalues']))
+    }
+    return event
+
+def delete_event(change):
+    event = {
+        'kind': 'delete',
+        'table': '{0}.{1}'.format(change['schema'], change['table']),
+        'types': dict(zip(change['oldkeys']['keynames'], change['oldkeys']['keytypes'])),
+        'old': dict(zip(change['oldkeys']['keynames'], change['oldkeys']['keyvalues'])),
+        'new': None
+    }
+    return event
+
+def msg_to_events_generator(msg):
     obj = json.loads(msg.payload)
-    logger.info('parsed message %s', obj)
+    if 'change' in obj:
+        for change in obj['change']:
+            event = None
+            if change['kind'] == 'insert':
+                event = insert_event(change)
+            elif change['kind'] == 'update':
+                event = update_event(change)
+            elif change['kind'] == 'delete':
+                event = delete_event(change)
+            if event:
+                event['sent_at'] = msg.send_time
+                event['lsn'] = msg.data_start
+                yield event
+
+def process_event_stdout(event):
+    print(json.dumps(event, sort_keys=True, default=str))
+
+def consume_stream(msg, whitelist_regex_c, blacklist_regex_c, process_event_fn):
+    logger.debug('got payload %s', msg.payload)
+    for event in msg_to_events_generator(msg=msg):
+        allowed = True
+        if whitelist_regex_c:
+            if not re.match(whitelist_regex_c, event['table']):
+                logger.debug('did not pass whitelist %s', event['table'])
+                allowed = False
+        if allowed and blacklist_regex_c:
+            if re.match(blacklist_regex_c, event['table']):
+                logger.debug('matched blacklist %s', event['table'])
+                allowed = False
+        if allowed:
+            process_event_fn(event=event)
+        else:
+            logger.debug('skipping event %s', event)
     msg.cursor.send_feedback(flush_lsn=msg.data_start)
 
 def init_cursor(pghost, pgport, pgdatabase, pguser, pgpassword, pgslot):
     conn = psycopg2.connect(f'host={pghost} port={pgport} dbname={pgdatabase} user={pguser} password={pgpassword}',
                                          connection_factory=LogicalReplicationConnection)
     cur = conn.cursor()
-    logger.info('trying to create replication slot %s', pgslot)
+    logger.debug('trying to create replication slot %s', pgslot)
     try:
         cur.create_replication_slot(slot_name=pgslot, slot_type=psycopg2.extras.REPLICATION_LOGICAL,
                                             output_plugin='wal2json')
@@ -27,10 +89,10 @@ def init_cursor(pghost, pgport, pgdatabase, pguser, pgpassword, pgslot):
         if err.pgcode != psycopg2.errorcodes.DUPLICATE_OBJECT:
             raise
         else:
-            logger.info('slot already exists, reusing')
-    logger.info('start replication')
+            logger.debug('slot already exists, reusing')
+    logger.debug('start replication')
     cur.start_replication(slot_name=pgslot, decode=True)
-    logger.info('started consuming')
+    logger.debug('started consuming')
     return cur
 
 @click.command()
@@ -40,10 +102,14 @@ def init_cursor(pghost, pgport, pgdatabase, pguser, pgpassword, pgslot):
 @click.option('--pguser', default=lambda: os.environ.get('PGUSER', None), required=True, help='Postgresql User ($PGUSER)')
 @click.option('--pgpassword', default=lambda: os.environ.get('PGPASSWORD', None), required=True, help='Postgresql Password ($PGPASSWORD)')
 @click.option('--pgslot', default=lambda: os.environ.get('PGSLOT', None), required=True, help='Postgresql Replication Slot Name ($PGSLOT)')
-def stdout(pghost, pgport, pgdatabase, pguser, pgpassword, pgslot):
-    logging.basicConfig(level=logging.DEBUG)
+@click.option('--whitelist-regex', required=False, help='Regex of schema.table to include - e.g. .*\.foo')
+@click.option('--blacklist-regex', required=False, help='Regex of schema.table to exclude - e.g. testns\..*')
+def stdout(pghost, pgport, pgdatabase, pguser, pgpassword, pgslot, whitelist_regex, blacklist_regex):
+    logging.basicConfig(level=logging.INFO)
     cur = init_cursor(pghost=pghost, pgport=pgport, pgdatabase=pgdatabase, pguser=pguser, pgpassword=pgpassword, pgslot=pgslot)
-    cur.consume_stream(consume_stream_stdout)
+    whitelist_regex_c = re.compile(whitelist_regex) if whitelist_regex else None
+    blacklist_regex_c = re.compile(blacklist_regex) if blacklist_regex else None
+    cur.consume_stream(consume=lambda msg : consume_stream(msg=msg, whitelist_regex_c=whitelist_regex_c, blacklist_regex_c=blacklist_regex_c, process_event_fn=process_event_stdout))
 
 @click.command()
 @click.option('--pghost', default=lambda: os.environ.get('PGHOST', None), required=True, help='Postgresql Host ($PGHOST)')
