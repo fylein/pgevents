974ca70b96aafc8ed02ad57f61b46d0fcf23d325
snarayanank2 <siva@fyle.in>
2020-11-18 11:45:25 +0530

    Clean event

--- rabbitmq_to_stdout.py
+++ rabbitmq_to_stdout.py
@@ -1,9 +1,30 @@
 import os
 import click
 import logging
+import json
 from libs.rabbitmq import create_rabbitmq_channel
 from libs.logging import init_logging
 logger = logging.getLogger(__name__)
+
+def clean_dict(d):
+    for k in d:
+        v = d[k]
+        if v is None:
+            del v[k]
+    return d
+
+def clean_event(event):
+    event['types'] = None
+    if event['kind'] == 'update':
+        event['old'] = None
+        event['new'] = None
+    if event['old']:
+        event['old'] = clean_dict(event['old'])
+    if event['new']:
+        event['new'] = clean_dict(event['new'])
+    if event['diff']:
+        event['diff'] = clean_dict(event['diff'])
+    return event
 
 @click.command()
 @click.option('--rabbitmq-url', default=lambda: os.environ.get('RABBITMQ_URL', None), required=True, help='RabbitMQ url ($RABBITMQ_URL)')
@@ -14,14 +35,16 @@ logger = logging.getLogger(__name__)
 def rabbitmq_to_stdout(rabbitmq_url, rabbitmq_exchange, binding_keys, queue_name, log_level):
     init_logging(log_level)
     rabbitmq_channel = create_rabbitmq_channel(rabbitmq_url=rabbitmq_url, rabbitmq_exchange=rabbitmq_exchange)
-    result = rabbitmq_channel.queue_declare(queue_name, durable=True, exclusive=False, auto_delete=False)
+    result = rabbitmq_channel.queue_declare(queue_name, durable=True, exclusive=True, auto_delete=True)
     queue_name = result.method.queue
     for binding_key in binding_keys.split(','):
         logging.info('binding to exchange %s, queue %s, binding_key %s', rabbitmq_exchange, queue_name, binding_key)
         rabbitmq_channel.queue_bind(exchange=rabbitmq_exchange, queue=queue_name, routing_key=binding_key)
     def callback(ch, method, properties, body):
 #        logger.info("[received] %r:%r" % (method.routing_key, body))
-        print(f'[received] {method.routing_key} {body}')
+        event = json.loads(body)
+        event = clean_event(event)
+        print(f'[received] {method.routing_key} {event}')
     rabbitmq_channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
     rabbitmq_channel.start_consuming()
 
