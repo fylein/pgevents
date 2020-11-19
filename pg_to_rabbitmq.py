21b8145607914cc23f7426ff8eb0bd0c619d4907
snarayanank2 <siva@fyle.in>
2020-11-19 06:44:01 +0530

    pgtables is not a required field

--- pg_to_rabbitmq.py
+++ pg_to_rabbitmq.py
@@ -26,7 +26,7 @@ def process_event_rabbitmq(rabbitmq_exchange, rabbitmq_channel, event):
 @click.option('--pguser', default=lambda: os.environ.get('PGUSER', None), required=True, help='Postgresql User ($PGUSER)')
 @click.option('--pgpassword', default=lambda: os.environ.get('PGPASSWORD', None), required=True, help='Postgresql Password ($PGPASSWORD)')
 @click.option('--pgslot', default=lambda: os.environ.get('PGSLOT', None), required=True, help='Postgresql Replication Slot Name ($PGSLOT)')
-@click.option('--pgtables', default=None, required=True, help='Restrict to specific tables e.g. public.transactions,public.reports')
+@click.option('--pgtables', default=None, required=False, help='Restrict to specific tables e.g. public.transactions,public.reports')
 @click.option('--rabbitmq-url', default=lambda: os.environ.get('RABBITMQ_URL', None), required=True, help='RabbitMQ url ($RABBITMQ_URL)')
 @click.option('--rabbitmq-exchange', default=lambda: os.environ.get('RABBITMQ_EXCHANGE', None), required=True, help='RabbitMQ exchange ($RABBITMQ_EXCHANGE)')
 def pg_to_rabbitmq(pghost, pgport, pgdatabase, pguser, pgpassword, pgslot, pgtables, rabbitmq_url, rabbitmq_exchange):
