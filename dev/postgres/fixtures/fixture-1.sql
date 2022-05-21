4259530997a5d5f61f54f458388e4643c4c2d0b9
snarayanank2 <siva@fyle.in>
2022-05-21 20:11:41 +0530

    Save

--- dev/postgres/fixtures/fixture-1.sql
+++
@@ -1,7 +0,0 @@
-create table users (
-    id int primary key,
-    full_name text not null
-);
-
-alter table users replica identity full;
-select pg_create_logical_replication_slot('pgevents', 'wal2json');
