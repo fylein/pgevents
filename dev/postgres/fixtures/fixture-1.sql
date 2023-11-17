be614ae3c546b4baabc5ada322f61bb47aae18dd
Shwetabh Kumar <shwetabh.kumar@fyle.in>
2023-11-17 17:49:18 +0530

    fix: Create publication in the fixture

--- dev/postgres/fixtures/fixture-1.sql
+++ dev/postgres/fixtures/fixture-1.sql
@@ -1,6 +1,11 @@
 create table users (
     id int primary key,
-    full_name text not null
+    full_name text not null,
+    company jsonb default '{"name": "Fyle"}'::jsonb,
+    created_at timestamp with time zone default now(),
+    updated_at timestamp with time zone default now()
 );
 
 alter table users replica identity full;
+
+create publication events for all tables;
