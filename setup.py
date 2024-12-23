85cb90bd44fd5fabd6a9eb1b258822204a99b312
ashwin1111 <winash1998@gmail.com>
2024-12-23 22:58:47 +0530

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pgevents',
-    version='0.1',
+    version='0.2',
     packages=find_packages(
         include=['src', 'producer', 'consumer']
     ),
