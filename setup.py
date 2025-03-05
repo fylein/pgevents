60063d20f9cbd812a0a5a98167c8f392295a1283
Ashutosh singh <55102089+Ashutosh619-sudo@users.noreply.github.com>
2025-03-06 00:23:11 +0530

    Feat: Add install_requires (#32)
    Co-authored-by: Ashutosh619-sudo <ashutosh.s@fyle.com>

--- setup.py
+++ setup.py
@@ -6,6 +6,11 @@ setup(
     packages=find_packages(
         include=['src', 'producer', 'consumer', 'common*']
     ),
+    install_requires=[
+        'Brotli==1.1.0',
+        'Click==8.1.8',
+        'pika==1.3.2',
+    ],
     entry_points='''
         [console_scripts]
         producer=producer.main:produce
