224cb79840b88c92abe1ef9bf87b6cc2d2e7bcfc
Ashutosh619-sudo <ashutosh.s@fyle.com>
2025-03-25 02:08:21 +0530

    Feat: Skip manual updates

---
+++ pgoutput_parser/update.py
@@ -0,0 +1,40 @@
+from common.log import get_logger
+
+from .base import BaseMessage
+
+
+logger = get_logger(__name__)
+
+
+
+class UpdateMessage(BaseMessage):
+    """Class for decoding PostgreSQL logical replication update messages."""
+
+    def decode_update_message(self) -> dict:
+        """
+        Decode an update message from the replication stream.
+
+        :return: A dictionary containing the decoded update message.
+        """
+        if self.message_type == 'U':
+            message_type = self.message_type
+            relation_id = self.relation_id
+
+            old_tuple = self.read_utf_8(length=1)
+            old_tuple_values = self.decode_tuple()
+            new_tuple = self.read_utf_8(length=1)
+            new_tuple_values = self.decode_tuple()
+
+            diff = self.calculate_diff(old_tuple_values, new_tuple_values)
+            if not diff or not new_tuple_values or not diff.get('updated_at'):
+                return None
+
+            return {
+                'table_name': self.table_name,
+                'id': new_tuple_values.get('id') or old_tuple_values.get('id'),
+                'old': old_tuple_values,
+                'new': new_tuple_values,
+                'diff': diff,
+                'action': self.message_type,
+                'recorded_at': self.recorded_at.isoformat()
+            }
