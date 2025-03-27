a29ff21863ca8c897e5db58a9ec03473b380e5f1
Ashutosh619-sudo <ashutosh.s@fyle.com>
2025-03-27 12:37:55 +0530

    bump up version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pgevents',
-    version='0.3',
+    version='0.4',
     packages=find_packages(
         include=['src', 'producer', 'consumer', 'common*']
     ),
